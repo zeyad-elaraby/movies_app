a5b72578946216e3357c77abc9842153f63bba96
ZEYAD <zeyadelaraby1@gmail.com>
2024-08-31 15:57:56 +0300

    app init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home_screen.dart';
+
+void main(){
+runApp(MyApp());
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes:{
+        HomeScreen.routeName:(context)=>HomeScreen(),
+      }
+
+    );
+  }
+}
